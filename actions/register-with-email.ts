4a59ff34e864454a33f8da8f6a7a20ad3a93edf4
Mikael Boutin <1322006+DWboutin@users.noreply.github.com>
2025-02-02 18:34:05 -0500

    vercel url

--- actions/register-with-email.ts
+++ actions/register-with-email.ts
@@ -4,7 +4,9 @@ import { createClient } from "@/utils/supabase/server";
 
 export const registerWithEmail = async (email: string) => {
   const supabase = await createClient();
-  const currentOrigin = process.env.NEXT_PUBLIC_CURRENT_ORIGIN;
+  const currentOrigin = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : process.env.NEXT_PUBLIC_CURRENT_ORIGIN;
 
   const response = await supabase.auth.signInWithOtp({
     email,
